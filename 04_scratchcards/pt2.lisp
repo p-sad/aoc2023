59661778bc0d048993f1c5d80f1d3ac8488bc00a
p-sad <sadlik512@gmail.com>
2023-12-04 22:52:38 +0100

    Rename lisp functions to try having some semblance of readability
    (Also remove TS config files since I don't need them)

--- 04_scratchcards/pt2.lisp
+++ 04_scratchcards/pt2.lisp
@@ -2,76 +2,76 @@
 
 (require 'uiop)
 
-(defun to_numbers (l)
+(defun strings_to_ints (l)
 ;; (str) => (int)
   (if (endp l)
     nil
     (if (string= "" (car l))
-      (to_numbers (cdr l))
+      (strings_to_ints (cdr l))
       (cons
         (parse-integer (car l))
-        (to_numbers (cdr l))))))
+        (strings_to_ints (cdr l))))))
 
-(defun to_list_of_numbers (str)
+(defun string_to_list_of_ints (str)
 ;; str => (int)
-  (to_numbers (uiop:split-string str :separator " ")))
+  (strings_to_ints (uiop:split-string str :separator " ")))
 
 (defun to_card (line)
 ;; str => (amount:int winning_numbers:(int) your_numbers:(int))
   (cons
     1
     (mapcar
-      'to_list_of_numbers
+      'string_to_list_of_ints
       (uiop:split-string line :separator "|"))))
 
 (defun parse_line (line)
 ;; str => card
   (to_card (second (uiop:split-string line :separator ":" ))))
 
-(defun accumulate_points (winning_numbers numbers)
+(defun count_points (winning_numbers numbers)
 ;; ((int)(int)) => int
   (if (endp numbers)
     0
     (+
       (if (member (car numbers) winning_numbers) 1 0)
-      (accumulate_points winning_numbers (cdr numbers)))))
+      (count_points winning_numbers (cdr numbers)))))
 
 (defun get_points (card)
 ;; card => int
-  (accumulate_points (second card) (third card)))
+  (count_points (second card) (third card)))
 
-(defun increment_card (card amount)
+(defun increase_number_of_card_instances (card amount)
 ;; card => card
   (cons (+ amount (car card)) (cdr card)))
 
-(defun increment_subsequent_cards (cards &key times amount)
+(defun increase_number_of_subsequent_cards (cards &key times amount)
 ;; (card) => (card)
   (if (= times 0)
     cards
     (cons
-      (increment_card (car cards) amount)
-      (increment_subsequent_cards
+      (increase_number_of_card_instances (car cards) amount)
+      (increase_number_of_subsequent_cards
         (cdr cards)
         :times (- times 1)
         :amount amount))))
 
-(defun get_number_of_each_card (cards)
+(defun count_each_card (cards)
 ;; (card) => (int)
   (if (endp cards)
     nil
     (cons
       (car (car cards))
-      (get_number_of_each_card
-        (increment_subsequent_cards
+      (count_each_card
+        (increase_number_of_subsequent_cards
           (cdr cards)
           :times (get_points (car cards))
           :amount (car (car cards)))))))
 
-(defun sum_cards (cards)
-  (apply '+ (get_number_of_each_card cards)))
+(defun sum_card_counts (cards)
+  (apply '+ (count_each_card cards)))
 
 (write-line
   (format nil "~a"
-    (sum_cards
+    (sum_card_counts
       (mapcar 'parse_line
         (uiop:read-file-lines (car (uiop:command-line-arguments)))))))
