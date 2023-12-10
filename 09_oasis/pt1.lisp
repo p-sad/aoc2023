fd723277d4e6ebe83d3da87c601f338b937cfcff
p-sad <sadlik512@gmail.com>
2023-12-10 21:18:55 +0100

    Lisp 9-1

---
+++ 09_oasis/pt1.lisp
@@ -0,0 +1,55 @@
+#!/usr/bin/env -S sbcl --script
+
+(require 'uiop)
+
+(defun to_numbers (l)
+  (if (endp l)
+    nil
+    (if (string= "" (car l))
+      (to_numbers (cdr l))
+      (cons
+        (parse-integer (car l))
+        (to_numbers (cdr l))))))
+
+(defun to_list_of_numbers (str)
+  (to_numbers (uiop:split-string str :separator " ")))
+
+(defun get_differences (numbers)
+;; list(int) => list(int)
+  (if (endp (cdr numbers))
+    nil
+    (cons
+      (- (cadr numbers) (car numbers))
+      (get_differences (cdr numbers)))))
+
+(defun is_zero (a) (= 0 a)) ;; int => bool
+
+(defun get_lists_of_differences (numbers)
+;; list(int) => list(list(int))
+  (let ((differences (get_differences numbers)))
+    (if (every 'is_zero differences)
+      (list differences)
+      (cons
+        differences
+        (get_lists_of_differences differences)))))
+
+(defun get_sequence_of_differences (numbers)
+  (cons numbers (get_lists_of_differences numbers)))
+
+(defun carlast (l) (car (last l))) ;; list(obj) => obj
+
+(defun find_next_item (sequences)
+;; list(list(int)) => int
+  (if (endp (cdr sequences))
+    (carlast (car sequences))
+    (+
+      (carlast (car sequences))
+      (find_next_item (cdr sequences)))))
+
+(write-line
+  (format nil "~a"
+    (apply '+
+      (mapcar 'find_next_item
+        (mapcar 'get_sequence_of_differences
+          (mapcar 'to_list_of_numbers
+            (uiop:read-file-lines (car (uiop:command-line-arguments)))))))))
