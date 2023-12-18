db614f879a079224437b29a347f486bc90884b1f
p-sad <sadlik512@gmail.com>
2023-12-18 01:31:36 +0100

    Cook up some spaghetti

--- 13_mirrors/pt2.py
+++ 13_mirrors/pt2.py
@@ -18,41 +18,38 @@ def main(filename: str):
 
 
 def find_vertical_mirror(puzzle: list[str]) -> int | None:
-    symmetry_axes = [
-        axis for axis in range(1, len(puzzle[0]))
-        if is_symmetry_axis(axis, puzzle[0])
-    ]
+    symmetry_gen = (get_symmetry_axis(axis, puzzle[0], False) for axis in range(1, len(puzzle[0])))
+    symmetry_axes = [(axis, used_smudge) for axis, used_smudge in symmetry_gen if axis is not None]
     for i in range(1, len(puzzle)):
-        symmetry_axes = [
-            axis for axis in symmetry_axes
-            if is_symmetry_axis(axis, puzzle[i])
-        ]
+        symmetry_gen = (get_symmetry_axis(axis, puzzle[i], used_smudge) for axis, used_smudge in symmetry_axes)
+        symmetry_axes = [(axis, used_smudge) for axis, used_smudge in symmetry_gen if axis is not None]
+    symmetry_axes = [axis for axis, used_smudge in symmetry_axes if used_smudge]
     return symmetry_axes[0] if len(symmetry_axes) == 1 else None
 
 
 def find_horizontal_mirror(puzzle: list[str]) -> int | None:
     def get_vertical_line(index: int):
         return [line[index] for line in puzzle]
-    symmetry_axes = [
-        axis for axis in range(1, len(puzzle))
-        if is_symmetry_axis(axis, get_vertical_line(0))
-    ]
+    symmetry_gen = (get_symmetry_axis(axis, get_vertical_line(0), False) for axis in range(1, len(puzzle)))
+    symmetry_axes = [(axis, used_smudge) for axis, used_smudge in symmetry_gen if axis is not None]
     for i in range(1, len(puzzle[0])):
-        symmetry_axes = [
-            axis for axis in symmetry_axes
-            if is_symmetry_axis(axis, get_vertical_line(i))
-        ]
+        symmetry_gen = (get_symmetry_axis(axis, get_vertical_line(i), used_smudge) for axis, used_smudge in symmetry_axes)
+        symmetry_axes = [(axis, used_smudge) for axis, used_smudge in symmetry_gen if axis is not None]
+    symmetry_axes = [axis for axis, used_smudge in symmetry_axes if used_smudge]
     return symmetry_axes[0] if len(symmetry_axes) == 1 else None
 
 
-def is_symmetry_axis(index: int, line: str | list[str]) -> bool:
+def get_symmetry_axis(index: int, line: str | list[str], used_smudge: bool) -> tuple[int | None, bool]:
     left, right = index-1, index
     while left >= 0 and right < len(line):
         if line[left] != line[right]:
-            return False
+            if not used_smudge:
+                used_smudge = True
+            else:
+                return None, True
         left -= 1
         right += 1
-    return True
+    return index, used_smudge
 
 
 def get_puzzles(lines: Iterable[str]) -> list[list[str]]:
